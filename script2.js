71b4ed6945ed0bc9fa2b33a4c7c45d6159a68a8f
shifa-s11 <shifa221054074@akgec.ac.in>
2023-10-25 00:34:49 +0530

    end page

--- script2.js
+++ script2.js
@@ -2,21 +2,7 @@ const username = localStorage.getItem("username");
 if (username) {
     document.getElementById("username").textContent = username;
 }
-//end
-const end = document.getElementById("end");
-end.addEventListener("click",() =>{
-    return window.location.assign("/end.html")
-})
-function endtest(){
-end.click();
-    }      
-const timeLimit = 5*60*1000; 
 
-function starttime() {
-     setTimeout(() => {
-       endtest();
-    }, timeLimit);
-}
 const value = document.getElementsByClassName("value");
 const choices = Array.from(document.getElementsByClassName("choice"));
 const question = document.getElementById("ques");
@@ -32,42 +18,59 @@ const attempt = 0;
 const maxques = 12;
 const unattempt = maxques;
 let review = 0;
+//choices and score
+let selchoice = null; 
 choices.forEach(choice => {
-    choice.addEventListener("click",event =>{
-    if(!accans) return;
-    accans = false;
-    const sel = event.target;
-    const selans = sel.dataset["number"];
-    if(selans == currques.answer){
- sel.style.backgroundColor = "green";
- score += 4;
-    }
-    else{
-        sel.style.backgroundColor = "red";
-        score -= 1;
-     }
-    setTimeout( () =>{
-        sel.style.backgroundColor ="#efe9e9"
-        newques()
-    },5000)
-    document.getElementById("score").textContent = score;
-})
+    choice.addEventListener("click", event => {
+        if (!accans) return;
+        accans = false;
+        const sel = event.target;
+        selchoice = sel;
+        sel.style.backgroundColor = "blue"; 
+
+    });
 });
+document.getElementById("submit").addEventListener("click", () => {
+     {
+        const selans = selchoice.dataset["number"];
+        if (selans == currques.answer) {
+            selchoice.style.backgroundColor = "green";
+            score += 4;
+        } else {
+            selchoice.style.backgroundColor = "red";
+            score -= 1;
+        }
+    }
+ setTimeout(() => {
+selchoice.style.backgroundColor = "#efe9e9";
+newques()
+    },2000 )
+        selectedChoice = null;
+        document.getElementById("score").textContent = score;
+        localStorage.setItem("score", score);
+        attempt++;
+        unattempt--;
+    }
+);
+// const reviewbutton =
 
+//end
+const end = document.getElementById("end");
+end.addEventListener("click",() =>{
+    localStorage.setItem("score",score)
+    return window.location.assign("end.html")
+})
+function endtest(){
+end.click();
+    }      
+const timeLimit = 5*1000; 
 
+function starttime() {
+     setTimeout(() => {
+       endtest();
+    }, timeLimit);
+}
 
-// document.getElementById("score").textContent = `Total Score: ${score}`;
-// // score
-// const submit = document.getElementById("submit");
-// const reviewbutton = document.getElementById("review");
-// submit.addEventListener("click",()=> 
-// newques())
-// reviewbutton.addEventListener("click",() =>
-// newques(),
-// review++,
-
-// )
-// document.getElementById("reviewbutton").textContent = `Review: ${review}`;
 start = () => {
     score = 0;
     quesCount = 0;
@@ -90,12 +93,13 @@ newques = () => {
     availquest.splice(quesindex, 1);
     accans = true;
 } else {
-    return window.location.assign("/end.html")
+    return window.location.assign("end.html")
 }
 };
 document.getElementById("next").addEventListener("click",() =>
 newques(),
-starttime())
+)
+starttime(),
 
 
 
