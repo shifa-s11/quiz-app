1dc1b9b9139845cbe8b3acf7020fa7dab881c0f7
shifa-s11 <shifa221054074@akgec.ac.in>
2023-10-24 08:33:46 +0530

    score

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ if (username) {
 
 const choices = Array.from(document.getElementsByClassName("choice"));
 const question = document.getElementById("ques");
+const optionLabels = Array.from(document.querySelectorAll(".choice .option")); // Added option labels
 let currques = {};
 let accans = true;
 let score = 0;
@@ -14,8 +15,27 @@ let questions = [];
 
 const corr_point = 4;
 const incorr_point = -1;
+const unattemted = 0;
 const maxques = 12;
+const review = 0;
+document.getElementById("submit").addEventListener("click", () => {
+    accans = true;
+    choices.forEach(choice =>{
+        choice.addEventListener("click",event =>{
+            if(!accans) return;
+            accans = false;
+            const selchoice = event.target;
+            const selans = selchoice.dataset["number"];
+      
+    if(selans==currques.answer){
+        score += 4;
+    }
+    else{
+        score -= 1;
+    } 
+});document.getElementById("score").textContent = `Total Score: ${score}`;})
 
+})
 start = () => {
     score = 0;
     quesCount = 0;
@@ -23,17 +43,26 @@ start = () => {
     console.log(availquest);
     newques();
 };
-
 newques = () => {
+    if (quesCount < maxques) {
     quesCount++;
     const quesindex = Math.floor(Math.random() * availquest.length);
     currques = availquest[quesindex];
-    question.querySelector("h2").textContent = currques.question;
-    choices.forEach(choice => {
-        const number = choice.querySelector(".value").getAttribute("data-number");
-        choice.querySelector(".value").textContent = currques['choosen' + number];
+  question.querySelector("h2").textContent = `${quesCount}. ${currques.question}`;
+    choices.forEach((choice, index) => {
+        const label = choice.querySelector(".option");
+        const option = choice.querySelector(".value");
+        label.textContent = String.fromCharCode(65 + index); // A, B, C, D
+        option.textContent = currques["choosen" + (index +1)];
     });
+    availquest.splice(quesindex, 1);
+} else {
+    return window.location.assign("/end.html")
+}
 };
+document.getElementById("next").addEventListener("click",() =>
+newques())
+
 
 // fetch API
 fetch("https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple")
@@ -45,14 +74,15 @@ fetch("https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=
         questions = data.results.map(data => {
             const questdisp = {
                 question: data.question,
+                answer: data.correct_answer,
             };
 
             const choices = [...data.incorrect_answers];
             questdisp.answer = Math.floor(Math.random() * 3) + 1;
             choices.splice(questdisp.answer - 1, 0, data.correct_answer);
-            choices.forEach((choosen, index) => {
-                questdisp["choosen" + (index + 1)] = choosen;
-            });
+            for (let i = 1; i <= 4; i++) {
+                questdisp["choosen" + i] = choices[i - 1];
+            }
 
             return questdisp;
         });
