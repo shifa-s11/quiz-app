c31aad418128a37ff36c2f17536d33b17fd97efb
shifa-s11 <shifa221054074@akgec.ac.in>
2023-10-28 16:02:37 +0530

    changes

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 const username = localStorage.getItem("username");
-if (username) {
-    document.getElementById("username").textContent = username;
-}
+username && (
+    document.getElementById("username").textContent = username
+)
 
 const value = document.getElementsByClassName("value");
 const choices = Array.from(document.getElementsByClassName("choice"));
@@ -9,7 +9,6 @@ const question = document.getElementById("ques");
 const optionLabels = Array.from(document.querySelectorAll(".choice .option")); // Added option labels
 let currques = {};
 let accans = true;
-
 let quesCount = 0;
 let availquest = [];
 let questions = [];
@@ -20,19 +19,18 @@ let unattempt = maxques;
 let review = 0;
 //choices and score
 let selchoice = null; 
-choices.forEach(choice => {
+choices.forEach(choice => { 
     choice.addEventListener("click", event => {
         if (!accans) return;
+        if (selchoice) {
+            selchoice.style.backgroundColor = "#efe9e9";
+        }
         accans = false;
         const sel = event.target;
         selchoice = sel;
         sel.style.backgroundColor = "blue"; 
-        setTimeout(() => {
-            selchoice.style.backgroundColor = "#efe9e9";
-                },2000 )
     });
 });
-
 //submit
 document.getElementById("submit").addEventListener("click", () => {
      {
@@ -75,20 +73,24 @@ setTimeout(() => {
 //end
 const end = document.getElementById("end");
 end.addEventListener("click",() =>{
-    localStorage.setItem("score",score)
+    addScore(username, score);
     return window.location.assign("end.html")
 })
 function endtest(){
 end.click();
     }      
-const timeLimit = 5*60*1000; 
-
+const timeLimit = 5*1000*60; 
 function starttime() {
      setTimeout(() => {
        endtest();
     }, timeLimit);
 }
+// ques display
+const next = document.getElementById("next")
 
+next.addEventListener("click",() =>
+newques(),
+)
 start = () => {
     score = 0;
     quesCount = 0;
@@ -101,23 +103,26 @@ newques = () => {
     quesCount++;
     const quesindex = Math.floor(Math.random() * availquest.length);
     currques = availquest[quesindex];
-  question.querySelector("h2").textContent = `${quesCount}. ${currques.question}`;
+    const questionElement = document.querySelector("h2");
+    questionElement.textContent = `${quesCount}. ${currques.question}`;
+    questionElement.id = `ques${quesCount}`;
     choices.forEach((choice, index) => {
         const label = choice.querySelector(".option");
         const option = choice.querySelector(".value");
+
         label.textContent = String.fromCharCode(65 + index); // A, B, C, D
         option.textContent = currques["choosen" + (index +1)];
     });
     availquest.splice(quesindex, 1);
-    accans = true;
-} else {
+    accans = true;}
+   else if(quesCount==maxques-1){
+next.style.display = "none";
+   }
+else {
     return window.location.assign("end.html")
 }
 };
-document.getElementById("next").addEventListener("click",() =>
-newques(),
-)
-starttime();
+
 
 //timer
 let time = 5*60;
@@ -125,45 +130,23 @@ const timer = document.getElementById("timer");
 function timedisp (){
 const min = Math.floor(time / 60);
 const sec = (time % 60);
-timer.textContent = `Time Left: 0${min}:${sec}`;
+if(sec<10){
+    timer.textContent = `Time Left: 0${min}:0${sec}`;
 }
-const timerInterval = setInterval(() => {
-    if (time <= 0) {
-       timer.textContent = "TIME'S UP"
-    } else {
-        time--;
-        timedisp();
-    }
-}, 1000);
-timedisp();
+else{
+timer.textContent = `Time Left: 0${min}:${sec}`;
+}if(time<30){
+    timer.style.color = "red"
+}
+}
+function alertwindow () {
+    window.alert(`Hurry Up!! \n Test is going to end soon`)
+}
+setTimeout(() =>{
+    alertwindow()
+},270000)
 
 
-// fetch API
-fetch("https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data.results);
-        questions = data.results.map(data => {
-            const questdisp = {
-                question: data.question,
-                answer: data.correct_answer,
-            };
-
-            const choices = [...data.incorrect_answers];
-            questdisp.answer = Math.floor(Math.random() * 3) + 1;
-            choices.splice(questdisp.answer - 1, 0, data.correct_answer);
-            for (let i = 1; i <= 4; i++) {
-                questdisp["choosen" + i] = choices[i - 1];
-            }
-            return questdisp;
-        });
-        start();
-    })
-    .catch(error => {
-        console.error(error);
-    });
 // STATUS
 const container = document.getElementById("container")
 const statbutton = document.getElementById("statbutton");
@@ -185,6 +168,57 @@ function status2(){
 statbutton.addEventListener("click",() =>{
     status2()
 })
+// high Scores
+function save(highScores) {
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+function addScore(username, score) {
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    highScores.push({ name: username, score: score });
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(3);
+    save(highScores);
+}
+// fetch API
+document.addEventListener("DOMContentLoaded", function () {
+    const loader = document.getElementById("loader");
+    loader.style.display = "flex";
+  
+fetch("https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.results);
+        questions = data.results.map((data,index) => {
+            const questdisp = {
+                id : index+1,
+                question: data.question,
+                answer: data.correct_answer,
+            };
 
-
-
+            const choices = [...data.incorrect_answers];
+            questdisp.answer = Math.floor(Math.random() * 3) + 1;
+            choices.splice(questdisp.answer - 1, 0, data.correct_answer);
+            for (let i = 1; i <= 4; i++) {
+                questdisp["choosen" + i] = choices[i - 1];
+            }
+            return questdisp;
+        });
+        start();
+        loader.style.display = "none";
+        starttime();
+        const timerInterval = setInterval(() => {
+            if (time <= 1) {
+               timer.textContent = "TIME'S UP";
+            }  else {
+                time--;
+                timedisp();
+            }
+        }, 1000);
+    }
+    )
+    .catch(error => {
+        console.error(error);
+    });})
+  
